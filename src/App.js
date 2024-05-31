4c9e5c6ac606c785e7dba29276d40abf803bbfe1
Sanket Patil <sanketpatil2063@gmail.com>
2024-05-31 16:11:15 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBar from "./NavBar";
+import Shop from "./Shop";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <Shop />
+      </div>
+    </>
+  );
+}
+
+export default App;
